8592a295b4bcd662b25db16339a56393ec1dc521
Fambirachimwe <t.svaku@gmail.com>
2020-04-25 13:47:14 +0200

    Updates

---
+++ precache-manifest.ac40b6caa161ed67358fcf75de026e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c683edd1fe0a07f8a0cbfade3f397dac",
+    "url": "/deployment/index.html"
+  },
+  {
+    "revision": "386fc84d1a845e9af5ed",
+    "url": "/deployment/static/css/main.aadfa8ef.chunk.css"
+  },
+  {
+    "revision": "a5cce04b993d5812f560",
+    "url": "/deployment/static/js/2.8115615c.chunk.js"
+  },
+  {
+    "revision": "386fc84d1a845e9af5ed",
+    "url": "/deployment/static/js/main.307a095e.chunk.js"
+  },
+  {
+    "revision": "9055591a3f0af39309a3",
+    "url": "/deployment/static/js/runtime-main.8b37325f.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/deployment/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/deployment/static/media/shoping-bag.30f07071.svg"
+  }
+]);
